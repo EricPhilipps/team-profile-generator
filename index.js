9ba6122d43a98ed5c104f937c0353b02843c1f34
Eric <ericphilipps123@gmail.com>
2022-04-19 22:22:08 -0500

    Reworking structures of classes and how inquirer works

--- index.js
+++ index.js
@@ -1,50 +1,40 @@
 const inquirer = require('inquirer');
-const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern')
 
+const employees = [];
 
-// const name = getName();
-// const id = getId();
-// const email = getEmail();
-
-// const test = new Employee (name, id, email);
-
-// console.log(test.firstName, test.id, test.email);
-
-input (questions) {
-inquirer
-.prompt([
-    {
-        type: 'input',
-        name: 'firstName',
-        message: 'What is the name of this Employee?',
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: 'What is the ID of this Employee?',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is the email of this Employee?',
-    },
-    {
-        type: 'input',
-        name: 'officeNumber',
-        message: 'What is the office number of this Employee?',
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'What is the GitHub of this Employee?',
-    },
-    {
-        type: 'input',
-        name: 'school',
-        message: 'What is the ID of this Employee?',
-    },
-])
-.then((answer) => {
-    return ;
-})
+const addManager = () => {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'firstName',
+                message: 'What is the name of this Employee?',
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is the ID of this Employee?',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is the email of this Employee?',
+            },
+            {
+                type: 'input',
+                name: 'officeNumber',
+                message: 'What is the office number of this Employee?',
+            },
+        ])
+        .then((managerInfo) => {
+            const {firstName, id, email, officeNumber} = managerInfo
+            const manager = new Manager(firstName, id, email, officeNumber);
+            employees.push(manager);
+            console.log(manager);
+        })
 }
+
+addManager();
