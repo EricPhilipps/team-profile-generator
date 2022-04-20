af746c34ee3061b1daacea1ef5ad47ec8d2550b0
Eric <ericphilipps123@gmail.com>
2022-04-19 22:39:40 -0500

    Engineer class working

--- index.js
+++ index.js
@@ -36,4 +36,35 @@ const addManager = () => {
         })
 }
 
-addManager();
+const addEngineer = () => {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'firstName',
+                message: 'What is the name of this Employee?',
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is the ID of this Employee?',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is the email of this Employee?',
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: 'What is the GitHub of this Employee?',
+            },
+        ])
+        .then((EngineerInfo) => {
+            const engineer = new Engineer(EngineerInfo.firstName, EngineerInfo.id, EngineerInfo.email, EngineerInfo.github);
+            employees.push(engineer);
+            console.log(engineer);
+        })
+}
+
+addEngineer();
